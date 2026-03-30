ecf1b6c4f3326198a4fd2cb6eda0caf27d949436
gbq38u <gbmailp@gmail.com>
2026-03-30 08:39:55 +0300

    best-pracs

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM eclipse-temurin:17-jdk
-LABEL maintainer="gb" version="1.1.0" description="Java-ONBUILD"
-ENV SERVER_PORT=8080
-ONBUILD COPY target/*.jar /app/app.jar
-ONBUILD EXPOSE 8080
-ONBUILD CMD ["java", "-jar", "/app/app.jar"]
-
+FROM python:3.9-slim
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+ENV HELLO_MSG=Hello_from_Docker
+EXPOSE 80
+CMD ["python","app.py"]
--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from Advanced Dockerfile!"
+    # Считываем значение переменной окружения, заданной в Dockerfile
+    message = os.getenv("HELLO_MSG", "No message found")
+    return f"App says: {message}"
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8080"))
-    app.run(host="0.0.0.0", port=port)
+    # По умолчанию Flask слушает порт 5000
+    # Если хотите слушать 80, измените порт либо используйте gunicorn
+    app.run(host="0.0.0.0", port=5000)
